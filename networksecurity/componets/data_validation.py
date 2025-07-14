b220037cd7c140368f212eaf8a90dd18edbb5bb9
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-14 15:38:34 +0530

    adding schema

--- networksecurity/componets/data_validation.py
+++ networksecurity/componets/data_validation.py
@@ -2,10 +2,20 @@ from networksecurity.entity.artifact_entity import DataIngestionArtifact,DataVal
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exception.exception import NetwrokSecurityException    
 from networksecurity.logging.logger import logging
-
+from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
 from scipy.stats import ks_2samp
 import pandas as pd
 import numpy as np
 import os
 import sys
 from typing import List
+
+class DataValidation:
+    def __init__(self,data_ingestion_artifact : DataIngestionArtifact,data_validation_config : DataValidationConfig):
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config =  read_yaml_file(file_path=SCHEMA_FILE_PATH)   
+        except Exception as e:
+            raise NetwrokSecurityException(e,sys)
+        
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,6 @@
+import yaml
+from networksecurity.exception.exception import NetwrokSecurityException
+from networksecurity.logging.logger import logging
+import os,sys
+import numpy as np
+import pickle
