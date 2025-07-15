80c418be3edf5d2256e7b946539647c7ea493f17
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-15 15:45:53 +0530

    data validation

--- networksecurity/componets/data_validation.py
+++ networksecurity/componets/data_validation.py
@@ -9,6 +9,7 @@ import numpy as np
 import os
 import sys
 from typing import List
+from networksecurity.utils.main_utils.utils import read_yaml_file
 
 class DataValidation:
     def __init__(self,data_ingestion_artifact : DataIngestionArtifact,data_validation_config : DataValidationConfig):
@@ -17,5 +18,28 @@ class DataValidation:
             self.data_validation_config = data_validation_config
             self._schema_config =  read_yaml_file(file_path=SCHEMA_FILE_PATH)   
         except Exception as e:
+    
             raise NetwrokSecurityException(e,sys)
-        
+    @staticmethod  
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetwrokSecurityException(e,sys)
+    def validate_number_of_columns(self,dataframe : pd.DataFrame)->bool:
+        try:
+            pass
+        except Exception as e:
+            raise NetwrokSecurityException(e,sys)
+
+    def initiate_data_validation(self)->DataValidationArtifact:
+        try:
+            train_file_path = self.data_ingestion_artifact.trained_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
+
+            ## read the data from train and test 
+            train_dataframe = DataValidation.read_data(file_path=train_file_path)
+            test_dataframe = DataValidation.read_data(file_path=test_file_path) 
+        except Exception as e:
+            raise NetwrokSecurityException(e,sys)
+            
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -4,3 +4,11 @@ from networksecurity.logging.logger import logging
 import os,sys
 import numpy as np
 import pickle
+
+
+def read_yaml_file(file_path:str)->dict:
+    try:
+        with open(file_path,"rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetwrokSecurityException(e,sys)
