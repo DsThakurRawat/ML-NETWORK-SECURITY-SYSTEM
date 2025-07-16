9ba4da9bc8ac20b6a3bd358e0a103ecef45412da
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-16 13:45:06 +0530

    setting up mongo db data base again

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.componets.data_ingestion import DataIngestion
+from networksecurity.componets.data_validation import DataValidation
 from networksecurity.exception.exception import NetwrokSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -10,9 +11,17 @@ if __name__ == '__main__':
           trainingpipelineconfig = TrainingPipelineConfig()
           data_ingestion_config = DataIngestionConfig(trainingpipelineconfig)
           data_ingestion = DataIngestion(data_ingestion_config)
-          logging.info("Initiate  data ingestion")
+          logging.info("Initiate the  data ingestion")
           dataingestionartifact =  data_ingestion.initiate_data_ingestion()
           print(dataingestionartifact)
+          logging.info("Data ingestion completed")
+
+          data_validation_config = DataValidation(trainingpipelineconfig)
+          data_validation = DataValidation(data_ingestion_config,data_validation_config)
+          logging.info("Initiate  data validation")
+          data_validation_artifact = data_validation.initiate_data_validation(dataingestionartifact)
+          print(data_validation_artifact)
+          logging.info("Data validation completed")
           
      except Exception as e:
           raise NetwrokSecurityException(e,sys)
--- test_mongodb.py
+++ test_mongodb.py
@@ -1,13 +1,8 @@
-
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://divyanshthakur594:Admin123@cluster0.jpdnejy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+uri = "mongodb+srv://divyanshthakur594:Admin101@Cluster0.bz5u2fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
